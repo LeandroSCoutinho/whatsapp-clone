a2b8718f5df818f8978848aad5f332dc0f417ab5
Leandro Coutinho <franciscoleandrosilvacoutinho@gmail.com>
2023-03-11 20:27:06 -0300

    Add contact

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -185,6 +185,24 @@ export class WhatsAppController{
             e.preventDefault();
 
             let formData =  new FormData(this.el.formPanelAddContact);
+
+            let contact = new User(formData.get('email'));
+
+            contact.on('datachange', data=>{
+
+                if (data.name) {
+                     
+                    this._user.addContact(contact).then(()=>{
+
+                        this.el.btnClosePanelAddContact.click();
+                        console.info('Contato foi adicionado!');
+                    });
+
+                } else {
+                    console.error('Usuário não foi encontrado.');
+                }
+            });
+
          });
 
          this.el.contactsMessagesList.querySelectorAll('.contact-item').forEach(item =>{
